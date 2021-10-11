cd8fcd7b36e000a1375dc7238f87317584c08070
SahilSayani <sahilsayani7@gmail.com>
2021-10-11 23:38:22 +0530

    styled error messages

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -75,7 +75,8 @@ class Account{
     public function getError($error)
     {
         if(in_array($error,$this->errorArray)){
-            return $error;
+            // return $error;
+            return "<span class='errorMessage'>$error</span>";
         }
     }
 
